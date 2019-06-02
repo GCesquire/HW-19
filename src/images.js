a7e839b1caffbfde8673ab1f91b2f8e064e08028
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-02 19:50:31 -0400

    fifth commit

--- src/images.js
+++ src/images.js
@@ -2,13 +2,13 @@ import beth from './images/beth.png';
 import birdperson from './images/birdperson.png';
 import evilmorty from './images/evilmorty.png';
 import gianthead from './images/gianthead.png';
-import goldenford from './images/goldenford.png';
 import jerry from './images/jerry.png';
 import jessica from './images/jessica.png';
 import meeseeks from './images/meeseeks.png';
-import morty from './images/morty.png';
-import mr from './images/mr.png';
+import morty from './images/Morty.png';
+import picklerick from './images/picklerick.png';
 import rick from './images/rick.png';
+import squanchy from './images/squanchy.png';
 import summer from './images/summer.png';
 
 const images = [
@@ -16,13 +16,13 @@ const images = [
     birdperson,
     evilmorty,
     gianthead,
-    goldenford,
     jerry,
     jessica,
     meeseeks,
     morty,
-    mr,
+    picklerick,
     rick,
+    squanchy,
     summer
 ];
 
