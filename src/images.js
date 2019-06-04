e325a04fb6972323ff4cf0f6d7730dbee0bdd9f0
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-04 13:51:06 -0400

    Tweaks

--- src/images.js
+++ src/images.js
@@ -1,29 +1,31 @@
-import beth from './images/BethSmith.png';
-import birdperson from './images/Birdperson.png';
-import evilmorty from './images/EvilMorty.png';
-import gianthead from './images/GiantHead.png';
-import jerry from './images/JerrySmith.png';
-import jessica from './images/Jessica.png';
-import meeseeks from './images/MrMeeseeks.png';
-import morty from './images/MortySmith.png';
-import picklerick from './images/PickleRick.png';
-import rick from './images/Rick.png';
-import squanchy from './images/Squanchy.png';
-import summer from './images/SummerSmith.png';
+import BethSmith from './images/BethSmith.png';
+import BG from './images/BG.png';
+import Birdperson from './images/Birdperson.png';
+import EvilMorty from './images/EvilMorty.png';
+import GiantHead from './images/GiantHead.png';
+import JerrySmith from './images/JerrySmith.png';
+import Jessica from './images/Jessica.png';
+import MrMeeseeks from './images/MrMeeseeks.png';
+import MortySmith from './images/MortySmith.png';
+import PickleRick from './images/PickleRick.png';
+import Rick from './images/Rick.png';
+import Squanchy from './images/Squanchy.png';
+import SummerSmith from './images/SummerSmith.png';
 
 const images = [
-    beth,
-    birdperson,
-    evilmorty,
-    gianthead,
-    jerry,
-    jessica,
-    meeseeks,
-    morty,
-    picklerick,
-    rick,
-    squanchy,
-    summer
+    BethSmith,
+    BG,
+    Birdperson,
+    EvilMorty,
+    GiantHead,
+    JerrySmith,
+    Jessica,
+    MrMeeseeks,
+    MortySmith,
+    PickleRick,
+    Rick,
+    Squanchy,
+    SummerSmith
 ];
 
 export default images;
