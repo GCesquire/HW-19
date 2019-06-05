c7bb6423f7bb38eda4d7429174ed40ef9618e947
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-05 09:53:10 -0400

    Frontend Tweaks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 const Header = () => (
     <div className="banner text-center d-flex align-items-center justify-content-center">
-        <p className="m-0">Click on an image to earn points, but don't click on any one more than once!</p>
+        <p className="m-0">Click on an image to earn points, but don't click on any one more than once or you will lose!</p>
     </div>
 );
 
