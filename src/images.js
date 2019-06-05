df2cbb10e766c7002b19bbebd30a1b175b9ce2d4
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-05 10:20:41 -0400

    Frontend Tweaks

--- src/images.js
+++ src/images.js
@@ -1,29 +1,29 @@
 import BethSmith from './images/BethSmith.png';
 import Birdperson from './images/Birdperson.png';
+import Cromulon from './images/Cromulon.png';
 import EvilMorty from './images/EvilMorty.png';
-import GiantHead from './images/GiantHead.png';
 import Header from './images/Header.png';
 import JerrySmith from './images/JerrySmith.png';
 import Jessica from './images/Jessica.png';
 import MrMeeseeks from './images/MrMeeseeks.png';
 import MortySmith from './images/MortySmith.png';
 import PickleRick from './images/PickleRick.png';
-import Rick from './images/Rick.png';
+import RickSanchez from './images/RickSanchez .png';
 import Squanchy from './images/Squanchy.png';
 import SummerSmith from './images/SummerSmith.png';
 
 const images = [
     BethSmith,
     Birdperson,
+    Cromulon,
     EvilMorty,
-    GiantHead,
     Header,
     JerrySmith,
     Jessica,
     MrMeeseeks,
     MortySmith,
     PickleRick,
-    Rick,
+    RickSanchez ,
     Squanchy,
     SummerSmith
 ];
