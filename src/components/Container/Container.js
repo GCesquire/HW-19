eb8c5efea12b4bf3a6d30f3deaf4085b7fcf85ea
Giovanny Caquias <giovannycaquias.esq@gmail.com>
2019-06-05 10:09:59 -0400

    Frontend Tweaks

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -5,7 +5,7 @@ import Characters from '../Characters';
 const Container = props => (
     <div
         className={
-            props.shake ? 'container d-flex flex-wrap justify-content-center' : 'container d-flex flex-wrap justify-content-center'
+            props ? 'container d-flex flex-wrap justify-content-center' : 'container d-flex flex-wrap justify-content-center'
         }
     >
     {props.Characters.map((x, y) => <Characters name={x} key={y} clickEvent={props.clickEvent} />)}
